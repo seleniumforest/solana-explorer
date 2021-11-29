94db9c24bfb437215cf2156b88e808e917aefccf
Kirill Tronin <troninkirill@gmail.com>
2021-11-29 21:12:03 +0400

    change fetch to axios

--- src/tokenList.js
+++ src/tokenList.js
@@ -1,12 +1,13 @@
+import axios from "axios";
+
 let cache;
 
 export const getTokens = async () => {
     if (cache)
         return cache;
-        
-    let response = await fetch("https://decommas.io/gears/app/api/tokens", { mode: "no-cors" });
-    let json = await response.json();
-    let tokenlist = json.map(x => ({
+
+    let response = await axios.get("https://decommas.io/gears/app/api/tokens");
+    let tokenlist = response.data.map(x => ({
         address: x.address,
         symbol: x.symbol
     }));
