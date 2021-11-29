c9733758714dd0ef0b444b9bbd4dc001b425d418
Kirill Tronin <troninkirill@gmail.com>
2021-11-29 20:59:11 +0400

    no cors mode

--- src/tokenList.js
+++ src/tokenList.js
@@ -4,7 +4,7 @@ export const getTokens = async () => {
     if (cache)
         return cache;
         
-    let response = await fetch("https://decommas.io/gears/app/api/tokens");
+    let response = await fetch("https://decommas.io/gears/app/api/tokens", { mode: "no-cors" });
     let json = await response.json();
     let tokenlist = json.map(x => ({
         address: x.address,
